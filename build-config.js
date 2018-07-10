22b2e2bd24033f50b0e476834a92326eb079db07
Yesarela <yezarela@gmail.com>
2018-07-10 21:00:18 +0700

    add graphcool-upload plugin

--- build-config.js
+++ build-config.js
@@ -30,7 +30,8 @@ module.exports = {
 		'@ckeditor/ckeditor5-image/src/imageupload',
 		'@ckeditor/ckeditor5-link/src/link',
 		'@ckeditor/ckeditor5-list/src/list',
-		'@ckeditor/ckeditor5-paragraph/src/paragraph'
+		'@ckeditor/ckeditor5-paragraph/src/paragraph',
+		'ckeditor5-graphcool-upload/src/graphcoolupload'
 	],
 
 	// Editor config.

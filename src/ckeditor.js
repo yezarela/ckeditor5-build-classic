422dfe5a80129385a44e190fe035813b7c2dadb9
Yesarela <yezarela@gmail.com>
2018-07-11 00:46:55 +0700

    add plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,6 +10,7 @@ import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import CodePlugin from '@ckeditor/ckeditor5-basic-styles/src/code';
 import BlockquotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import EasyimagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
@@ -23,6 +24,8 @@ import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import AlignmentPlugin from '@ckeditor/ckeditor5-alignment/src/alignment';
 import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
+import TabletoolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TableutilsPlugin from '@ckeditor/ckeditor5-table/src/tableutils';
 import FontsizePlugin from '@ckeditor/ckeditor5-font/src/fontsize';
 import FontfamilyPlugin from '@ckeditor/ckeditor5-font/src/fontfamily';
 import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight';
@@ -38,6 +41,7 @@ ClassicEditor.build = {
 		BoldPlugin,
 		ItalicPlugin,
 		UnderlinePlugin,
+		CodePlugin,
 		BlockquotePlugin,
 		EasyimagePlugin,
 		HeadingPlugin,
@@ -51,6 +55,8 @@ ClassicEditor.build = {
 		ParagraphPlugin,
 		AlignmentPlugin,
 		TablePlugin,
+		TabletoolbarPlugin,
+		TableutilsPlugin,
 		FontsizePlugin,
 		FontfamilyPlugin,
 		HighlightPlugin,
